4dd8b4772b6b8895fa3443812d3466bb2ad72eb3
Evan Klein <me@evanklein.com>
2020-05-07 10:18:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zone File Example
+
+An example repository for the [Zone File docs](https://github.com/evan-klein/zone-file/blob/master/docs/generate-zone-file.sh.md).
---
+++ zone-file-generator.php
@@ -0,0 +1,12 @@
+<?php
+
+require(__DIR__ . '/../../repo/ZoneFile.php');
+
+$zone_file = new ZoneFile('example.com.');
+
+$zone_file->addA('www', '93.184.216.34');
+$zone_file->addAAAA('www', '2606:2800:220:1:248:1893:25c8:1946');
+
+echo $zone_file->output();
+
+?>
