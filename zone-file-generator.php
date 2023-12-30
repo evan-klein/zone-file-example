f05edf4ba18d09e48ab4bb1664b5a5edd9fc2eb1
Evan Klein <me@evanklein.com>
2023-12-30 12:17:25 -0800

    Progress commit; squash later

--- zone-file-generator.php
+++ zone-file-generator.php
@@ -2,7 +2,7 @@
 
 require(__DIR__ . '/../../repo/ZoneFile.php');
 
-$zone_file = new ZoneFile('example.com.');
+$zone_file = new \evan_klein\zone_file\ZoneFile('example.com.');
 
 $zone_file->addA('www', '93.184.216.34');
 $zone_file->addAAAA('www', '2606:2800:220:1:248:1893:25c8:1946');
